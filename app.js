8a26fc477f0fbea75953af1d1ddde8ee871aaf77
yassmine005 <yassminem458@gmail.com>
2025-08-25 11:50:49 +0100

    cérate server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const http =require("http")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -35,4 +36,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });// ken serveur ta7 
 
-module.exports = app;
+;
+const server = http.createServer(app)
+server.listen(5000,()=>{console.log("app is running on port 5000")})
