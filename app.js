2f75181918e2da5d4082b302f32bf2ed9fda12e8
yassmine005 <yassminem458@gmail.com>
2025-08-25 11:29:17 +0100

    delete view

--- app.js
+++ app.js
@@ -9,18 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('dev')); //bech t5raje al chaque requete detail el koul
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));//dossier public
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter);// kifech tousel li index router 
+app.use('/users', usersRouter);//kifech tousel el userrouter
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +33,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});// ken serveur ta7 
 
 module.exports = app;
