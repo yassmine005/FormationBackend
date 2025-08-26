81affb2b75d9119a6605e38e56d3e1e2be2359a2
yassmine005 <yassminem458@gmail.com>
2025-08-26 10:51:30 +0100

    cérate osRouteur.js

---
+++ routes/osRouteur.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const os = require("os");
+
+/* GET users listing. 
+router.get('/marhba', function(req, res, next) {
+  res.json('welcome');
+});*/ 
+const osController = require ("../Controllers/osContoller")
+// GET OS Information
+router.get('/getOsInformation', osController.getOsInformation) ;
+module.exports = router;
