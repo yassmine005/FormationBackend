f9ea1beddc54afaf7cae5ed2372f18eed95ae087
yassmine005 <yassminem458@gmail.com>
2025-08-28 19:08:53 +0100

    reliee avec controller

--- routes/pharmacyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const pharmacyController = require("../Controllers/pharmacyController");
-router.post("/", pharmacyController.addPharmacy);
-router.put("/:id", pharmacyController.updatePharmacy);
-router.get("/", pharmacyController.listPharmacies);
-
-module.exports = router;
