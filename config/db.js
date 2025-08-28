a230dab6642053684f4bcf12611de71dcbe0eb42
yassmine005 <yassminem458@gmail.com>
2025-08-28 19:07:05 +0100

    changement de link de base de donnee

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
+// connexion avec base de donnée en le faire une seul fois dans tout le projet 
 const { formatArgs } = require("debug/src/browser")
 const  mongoose=require("mongoose")
-
+//module.exports nom de fonction l'exportation de fonction 
 module.exports.connectToMongoDb = async () => {
     mongoose.set('strictQuery', false)
-    mongoose.connect("mongodb+srv://yassminem458:LTvetSsDqHWG0HWC@cluster0.u6aaknx.mongodb.net/").then(()=>{
+    mongoose.connect(process.env.url_Db)
+        .then(()=>{
         console.log("connect to db ");
     })
     .catch((error) => {
